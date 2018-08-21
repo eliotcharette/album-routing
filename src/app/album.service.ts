ace28d9b55e748f465adad5641764bd65c24c847
Guest User <Guest@epicodus-55.local>
2018-08-21 11:04:59 -0700

    Add firebase database functionality

---
+++ src/app/album.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class AlbumService {
+  albums: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) {
+    this.albums = database.list('albums');
+  }
+
+  getAlbums() {
+    return this.albums;
+  }
+
+  getAlbumById(albumId: number){
+    for (var i = 0; i <= ALBUMS.length - 1; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
+  }
+
+}
